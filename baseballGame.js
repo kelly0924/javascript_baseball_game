4bccfbb4fc9461e6c1f43c9493af96a7d9a2ebf6
kelly0924 <hyj09240@gmail.com>
2022-02-10 12:20:10 +0000

    add win or out

--- baseballGame.js
+++ baseballGame.js
@@ -4,24 +4,6 @@ var resultNum = " ";
 var ranNum1,ranNum2,ranNum3;
 var userNum1,userNum2,userNum3;
 
-function pickNumber(num){
-    var tmpNum=document.getElementById(num).value;
-   // console.log(document.getElementById("input_num1").innerHTML);
-    //var tmpInputNum=document.getElementById("input_num");
-    if( document.getElementById("input_num1").innerHTML==""){
-        resultNum = tmpNum;
-        document.getElementById("input_num1").innerHTML=tmpNum;
-        cnt=0;
-    }else if(document.getElementById("input_num2").innerHTML == ""){
-        resultNum = resultNum + tmpNum;
-        document.getElementById("input_num2").innerHTML=tmpNum;
-        cnt++; 
-    }else if(document.getElementById("input_num3").innerHTML==""){
-        resultNum = resultNum + tmpNum;
-        document.getElementById("input_num3").innerHTML=tmpNum;
-        cnt++; 
-    }   
-}
 //random 난 수 생성하기 
 window.onload=function(){ 
     ranNum1=Math.floor((Math.random()*9)+1);
@@ -35,6 +17,22 @@ window.onload=function(){
     }
     console.log(ranNum1,ranNum2,ranNum3);
 }
+
+function pickNumber(num){
+    var tmpNum=document.getElementById(num).value;
+   // console.log(document.getElementById("input_num1").innerHTML);
+    //var tmpInputNum=document.getElementById("input_num");
+    if( document.getElementById("input_num1").innerHTML==""){
+        resultNum = tmpNum;
+        document.getElementById("input_num1").innerHTML=tmpNum;
+    }else if(document.getElementById("input_num2").innerHTML == ""){
+        resultNum = resultNum + tmpNum;
+        document.getElementById("input_num2").innerHTML=tmpNum;
+    }else if(document.getElementById("input_num3").innerHTML==""){
+        resultNum = resultNum + tmpNum;
+        document.getElementById("input_num3").innerHTML=tmpNum;
+    }   
+}
 //random 숫자와 사용자가 입력한 숫가 같은지를 보는 함수 
 function throwNumbers(){
     var tmpInt=parseInt(resultNum);
@@ -45,6 +43,7 @@ function throwNumbers(){
 
     if(ranNum1 == userNum1 && ranNum2 == userNum2 && ranNum3 ==userNum3){
         document.getElementById("strike").innerHTML="3 strike!!";
+        document.getElementById("win_out").innerHTML="win! win! win! ";
     }else if(ranNum1 == userNum1){
         if(ranNum2 == userNum3 && ranNum3 == userNum2){
             document.getElementById("strike").innerHTML="1 strike";
@@ -87,9 +86,15 @@ function throwNumbers(){
     }else{
         document.getElementById("strike").innerHTML="no strike";
         document.getElementById("ball").innerHTML="no ball";
-
+        document.getElementById("win_out").innerHTML="you are out!";
+       
     }
 
 }
-
-
+// function deletBotton(){
+//     document.getElementById("input_num1").innerHTML=" ";
+//     document.getElementById("input_num2").innerHTML=" ";
+//     document.getElementById("input_num3").innerHTML=" ";
+//     resultNum=" ";
+    
+// }
